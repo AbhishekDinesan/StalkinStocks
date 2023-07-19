dcd5ac054a7d3cc03eb6157ded9e302b1eb5370f
Abhishek Dinesan <abhidines04@gmail.com>
2023-07-19 07:59:36 -0400

    Tailwind CSS, Stock Table

--- src/Stock.js
+++ src/Stock.js
@@ -103,10 +103,11 @@ class Stock extends React.Component{
     render(){
         const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
         return (
-    <div>
-         <h1>Stalkin' Stocks</h1>
+    <div class = "title-text">
+         <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Stalkin'</span>Stocks.</h1>
+         <h3 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-2xl dark:text-white">Choose from the dropdown to view <span class="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">stock data</span></h3>
          <h4>Stock Symbol: <span id="demo">MSFT</span></h4>
-         <h1>`This is ${stockName} recent performance`</h1>
+         <p class="text-sm font-normal text-gray-500 lg:sm dark:text-gray-400">Hey there! AlphaVantage API only supports 5 API calls/min upto 100 calls/day so please be patient!</p>
          <div className="form-group">
   <select
     className="form-control"
@@ -116,6 +117,8 @@ class Stock extends React.Component{
     <option value="MSFT">Microsoft (MSFT)</option>
     <option value="AAPL">Apple (AAPL)</option>
     <option value="GOOGL">Alphabet (GOOGL)</option>
+    <option value="IBM">IBM (IBM)</option>
+    <option value="AMZN">Amazon (AMZN)</option>
     {/* Add more options for other stocks */}
   </select>
   <button type="submit" className="form-control btn btn-whatever">
@@ -153,6 +156,46 @@ class Stock extends React.Component{
             }}/>
         </div>
     </div>
+    {
+
+    <body class = "bg-cyan-900 flex justify-center items-center h-screen" >
+        <table class = "shadow-2x1 font-[Poppins] border-2 border-cyan-200 w-6/12">
+            <thead class = "text-white">
+                <tr>
+                    <th class = "py-3 bg-cyan-800">Hey</th>
+                    <th class = "py-3 bg-cyan-800">There</th>
+                    <th class = "py-3 bg-cyan-800">Hows</th>
+                    <th class = "py-3 bg-cyan-800">It</th>
+                </tr>
+            </thead>
+            <tbody class = "text-cyan-900 text-center">
+                <tr class = "bg-cyan-200 cursor-pointer duration-300">
+                    <td class = "py-3 px-6"> 1</td>
+                    <td class = "py-3 px-6"> 2</td>
+                    <td class = "py-3 px-6"> 3</td>
+                    <td class = "py-3 px-6"> 4</td>
+                </tr>
+                <tr class = "bg-cyan-200 cursor-pointer duration-300">
+                    <td class = "py-3 px-6"> 5</td>
+                    <td class = "py-3 px-6"> 6</td>
+                    <td class = "py-3 px-6"> 7</td>
+                    <td class = "py-3 px-6"> 8</td>
+                </tr>
+                <tr class = "bg-cyan-200 cursor-pointer duration-300">
+                    <td class = "py-3 px-6"> 9</td>
+                    <td class = "py-3 px-6"> 10</td>
+                    <td class = "py-3 px-6"> 11</td>
+                    <td class = "py-3 px-6"> 12</td>
+                </tr>
+
+            </tbody>
+        </table>
+    </body>
+        }
+
+
+
+
 </div>
         )
     }
