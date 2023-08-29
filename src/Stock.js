05d242db153210afc2edf0768aff260247fa16a0
Abhishek Dinesan <abhidines04@gmail.com>
2023-08-29 19:28:06 -0400

    Candlestick, RangeSlider + Styling

--- src/Stock.js
+++ src/Stock.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Plot from 'react-plotly.js';
-import ReactDOM from 'react-dom';
+import { BsArrowLeftCircleFill } from "react-icons/bs";
 import './styling/Stocks.css';
 let stockName = "MSFT"; 
 const math = require('mathjs');
@@ -24,11 +24,9 @@ class Stock extends React.Component{
         }
         this.handleStockChange = this.handleStockChange.bind(this); // Bind the method
     }
-
     handleStockChange(event) {
         this.setState({ selectedStock: event.target.value });
       }
-
     componentDidMount (){
         this.fetchStock();
     }
@@ -59,17 +57,10 @@ class Stock extends React.Component{
                     stockChartXValuesFunction.push(key);
                     stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
                 }
-
                 pointerToThis.setState({
                     stockChartXValues: stockChartXValuesFunction,
                     stockChartYValues: stockChartYValuesFunction
-                });
-
-
-                if(!stockName){
-                    document.getElementById('demo').innerHTML= "Nothing available at this time";
-                }     
-
+                });    
             }
         )
     
@@ -82,32 +73,85 @@ class Stock extends React.Component{
     .then (
         function(data){
             console.log(data);
-        
             for (var key in data['Time Series (Daily)']){
                 stockChartXValuesFunctionFull.push(key);
                 stockChartYValuesFunctionFull.push(data['Time Series (Daily)'][key]['1. open']);
             }
-
             pointerToThis.setState({
                 stockChartXValuesFull: stockChartXValuesFunctionFull,
                 stockChartYValuesFull: stockChartYValuesFunctionFull
             });
-
-            if(!stockName){
-                document.getElementById('demo').innerHTML= "Nothing available at this time";
-            }     
-
         }
     )
     }
     render(){
-        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        const trace1 = {
+            x: this.state.stockChartXValuesFull,
+            close: this.state.stockChartYValuesFull,
+            decreasing: { line: { color: '##ff0000' } },
+            high: this.state.stockChartYValuesFull,
+            increasing: { line: { color: '#000000' } },
+            line: { color: 'rgba(31,119,180,1)' },
+            low: this.state.stockChartYValuesFull,
+            open: this.state.stockChartYValuesFull,
+            type: 'candlestick',
+            xaxis: 'x',
+            yaxis: 'y',
+          };
+      
+          const data = [trace1];
+      
+          const layout = {
+            dragmode: 'zoom',
+            margin: {
+              r: 10,
+              t: 25,
+              b: 40,
+              l: 60,
+            },
+            title: {
+                text: `20Y Candlestick for ${stockName}`, // Your desired title
+                x: 0.5,  // Adjust the horizontal alignment of the title
+                font: {
+                  size: 16,  // Adjust the font size of the title
+                  color: 'black',
+                  weight: 'bold',
+                },
+              },
+            showlegend: false,
+            xaxis: {
+              autorange: true,
+              domain: [0, 1],
+              range: [
+                this.state.stockChartXValues[0],
+                this.state.stockChartXValuesFull[this.state.stockChartXValuesFull.length - 1]
+              ],
+              rangeslider: {
+                range: [
+                  this.state.stockChartXValuesFull[0],
+                  this.state.stockChartXValuesFull[this.state.stockChartXValuesFull.length - 1]
+                ],
+              },
+              type: 'date',
+            },
+            yaxis: {
+              autorange: true,
+              domain: [0, 1],
+              range: [
+                Math.min(...this.state.stockChartYValuesFull),
+                Math.max(...this.state.stockChartYValuesFull)
+              ],
+              type: 'linear',
+            },
+            paper_bgcolor: 'rgba(0, 0, 0, 0)',    // Make the paper background transparent
+        plot_bgcolor: 'rgba(0, 0, 0, 0)',
+          };
         return (
+            
     <div class = "title-text">
          <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-black from-red-600">Stalkin'</span>Stocks.</h1>
-         <h3 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-2xl dark:text-white">Choose from the dropdown to view <span class="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">stock data</span></h3>
+         <h3 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-2xl dark:text-white">Choose from the dropdown to view <span class="underline underline-offset-3 decoration-8 decoration-red-400 dark:decoration-red-600">stock data</span></h3>
          <h4>Stock Symbol: <span id="demo">MSFT</span></h4>
-         <p class="text-sm font-normal text-gray-500 lg:sm dark:text-gray-400">Hey there! AlphaVantage API only supports 5 API calls/min upto 100 calls/day so please be patient!</p>
          <div className="form-group">
   <select
     className="form-control"
@@ -124,10 +168,14 @@ class Stock extends React.Component{
   <button type="submit" className="form-control btn btn-whatever">
     Select Stock
   </button>
+
+
+
 </div>
     <div className="card-container">        
     <div className="card">
-        <Plot
+        
+    <Plot
             data={[
              {
                 x: this.state.stockChartXValues,
@@ -139,62 +187,26 @@ class Stock extends React.Component{
         ]}
         
             layout={{width: 620, height: 440,  paper_bgcolor: 'rgba(0,0,0,0)',
-            plot_bgcolor: 'rgba(0,0,0,0)' }}/>
+            plot_bgcolor: 'rgba(0,0,0,0)' ,
+            title: {
+                text: `6M Scatter for ${stockName}`, // Your desired title
+                x: 0.5,  // Adjust the horizontal alignment of the title
+                font: {
+                  size: 16,  // Adjust the font size of the title
+                  color: 'black',
+                  weight: 'bold',
+                },
+              },
+            }}/>
     </div>
         <div className="card">
+        
         <Plot
-            data={[
-             {
-                x: this.state.stockChartXValuesFull,
-                y: this.state.stockChartYValuesFull,
-                type: 'scatter',
-                mode: 'lines+markers',
-                marker: { color: 'red' },
-            }
-        ]}
-            layout={{width: 620, height: 440, paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)',
-            }}/>
+        data={data}
+        layout={layout}
+            />
         </div>
     </div>
-    {
-
-    <body class = "bg-cyan-900 flex justify-center items-center h-screen" >
-        <table class = "shadow-2x1 font-[Poppins] border-2 border-cyan-200 w-6/12">
-            <thead class = "text-white">
-                <tr>
-                    <th class = "py-3 bg-cyan-800">Hey</th>
-                    <th class = "py-3 bg-cyan-800">There</th>
-                    <th class = "py-3 bg-cyan-800">Hows</th>
-                    <th class = "py-3 bg-cyan-800">It</th>
-                </tr>
-            </thead>
-            <tbody class = "text-cyan-900 text-center">
-                <tr class = "bg-cyan-200 cursor-pointer duration-300">
-                    <td class = "py-3 px-6"> 1</td>
-                    <td class = "py-3 px-6"> 2</td>
-                    <td class = "py-3 px-6"> 3</td>
-                    <td class = "py-3 px-6"> 4</td>
-                </tr>
-                <tr class = "bg-cyan-200 cursor-pointer duration-300">
-                    <td class = "py-3 px-6"> 5</td>
-                    <td class = "py-3 px-6"> 6</td>
-                    <td class = "py-3 px-6"> 7</td>
-                    <td class = "py-3 px-6"> 8</td>
-                </tr>
-                <tr class = "bg-cyan-200 cursor-pointer duration-300">
-                    <td class = "py-3 px-6"> 9</td>
-                    <td class = "py-3 px-6"> 10</td>
-                    <td class = "py-3 px-6"> 11</td>
-                    <td class = "py-3 px-6"> 12</td>
-                </tr>
-
-            </tbody>
-        </table>
-    </body>
-        }
-
-
-
 
 </div>
         )
